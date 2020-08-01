56a9219864d63b5465b812e9c36111b50df94057
jack456054 <jack456054@hotmail.com>
2020-08-01 16:33:58 +0800

    feat(texas_poker.py): finish distributing cards

--- texas_poker.py
+++ texas_poker.py
@@ -1,47 +1,61 @@
 import random
 
 
+class Player():
+
+    def __init__(self):
+        self.cards = []
+        self.rank = ()
+        self.rank_value = []
+
+
 class Game():
 
     def __init__(self):
-        self.remain_cards = {}
-        self.cards_on_board = {}
+        self.remain_cards = []
+        self.cards_on_board = []
         self.card_faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         self.players = {}
+        self.all_ranks = {
+            'straight_flush': 9,
+            'four_of_a_kind': 8,
+            'full_house': 7,
+            'flush': 6,
+            'straight': 5,
+            'three_of_a_kind': 4,
+            'two_pairs': 3,
+            'one_pair': 2,
+            'nothing': 1,
+        }
 
-    def shuffle_cards(self):
+    def shuffle_cards(self) -> None:
         for suit in ['spade', 'heart', 'diamond', 'club']:
             for index, face in enumerate(self.card_faces):
-                self.remain_cards[suit + '_' + str(face)] = index + 1
+                self.remain_cards.append((suit, index + 1))
 
-    def distribute_a_random_card(self):
+    def distribute_a_random_card(self) -> None:
         for player in self.players.values():
-            card = random.choice(list(self.remain_cards.keys()))
-            player.cards[card] = self.remain_cards[card]
-            del self.remain_cards[card]
+            card = random.choice(self.remain_cards)
+            self.remain_cards.remove(card)
+            player.cards.append(card)
 
     # Distribute a specific card to the player.
     def distribute_a_card(self, player, card):
         pass
 
-    def distribute_a_random_card_on_board(self):
-        card = random.choice(list(self.remain_cards.keys()))
-        self.cards_on_board[card] = self.remain_cards[card]
-        del self.remain_cards[card]
+    def distribute_a_random_card_on_board(self) -> None:
+        card = random.choice(self.remain_cards)
+        self.remain_cards.remove(card)
+        self.cards_on_board.append(card)
 
-    def set_players(self, number_of_players):
+    def set_players(self, number_of_players: int) -> None:
         for number in range(number_of_players):
-            self.players[f'Player_{number}'] = self.Player()
+            self.players[f'Player_{number}'] = Player()
 
-    def show_cards(self):
-        for (key, value) in self.players.items():
-            print(f'{key}: {value.cards}')
+    def show_cards(self) -> None:
+        for (player_number, player) in self.players.items():
+            print(f'{player_number}: {player.cards}')
         print(f'Cards on board: {self.cards_on_board}')
-
-    class Player():
-
-        def __init__(self):
-            self.cards = {}
 
 
 if __name__ == "__main__":
